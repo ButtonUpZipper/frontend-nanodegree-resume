56f961d456dbb84c87f7512461933ae29cd69afd
Greg <greg_555_@hotmail.com>
2017-04-24 16:15:56 -0400

    ADD - Lesson 3 complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,108 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
+
+
+$("#main").append([string]);
+[string].replace([old],[new]);
 */
-$("#main").append("Greg");
+
+
+
+//BIO
+var bio = {
+  "name" : "Greg Hunt",
+  "role" : "Phthalo Green Wizard",
+  "welcomeMessage" : "Welcome To The Greg Hunt Experience!",
+  "biopic" : "images/me.jpg",
+  "contacts" : {
+    "mobile" : "mPHone#",
+    "email" : "email@email.com",
+    "github" : "github username",
+    "twitter" : "twitter handle",
+    "location" : "Awesome City"
+  },
+  "skills" : [
+    "awesomeness",
+    "programming",
+    "teaching",
+    "JS"
+  ]
+};
+
+
+
+//WORK
+var work = {
+  "jobs" : [
+    {
+      "employer" : "The Dark One",
+      "title" : "Wizard",
+      "location" : "Mordor",
+      "dates" : "4",
+      "description" : "Cast spells"
+    },
+    {
+      "employer" : "The Dark One",
+      "title" : "Wizard",
+      "location" : "Mordor",
+      "dates" : "4",
+      "description" : "Cast spells"
+    }
+  ]
+};
+
+
+
+//PROJECTS
+var projects = {
+  "projects" : [
+    {
+      "title" : "Magic Missle Project",
+      "dates" : "200-250",
+      "description" : "Fires multiple projectiles",
+      "images" : ["images/fry.jpg", "images/fry.jpg"]
+    },
+    {
+      "title" : "Magic Missle Project",
+      "dates" : "200-250",
+      "description" : "Fires multiple projectiles",
+      "images" : ["images/fry.jpg", "images/fry.jpg"]
+    }
+  ]
+};
+
+
+
+//EDUCATION
+var education = {
+  "schools" : [
+    {
+      "name" : "Orthanc (satellite campus)",
+      "location" : "School City",
+      "degreeDates" : "200-250",
+      "url" : "www.school.com",
+      "majors" : ["major1","major2"]
+    },
+    {
+      "name" : "Orthanc (satellite campus)",
+      "location" : "School City",
+      "degreeDates" : "200-250",
+      "url" : "www.school.com",
+      "majors" : ["major1","major2"]
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Web Stuff",
+      "school" : "Online School",
+      "dates" : "1600-4585",
+      "url" : "www.coolschool.com"
+    },
+    {
+      "title" : "Web Stuff",
+      "school" : "Online School",
+      "dates" : "1600-4585",
+      "url" : "www.coolschool.com"
+    }
+  ]
+};
